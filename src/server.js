45a0fa83923a3ed3e6cf2eefc8041bc1ea5349b3
maqss <maksymgulko@gmail.com>
2025-01-06 13:32:12 +0100

    change the status and message display

--- src/server.js
+++ src/server.js
@@ -24,9 +24,9 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
-      data: contacts,
       status: 200,
       message: 'Successfully found contacts!',
+      data: contacts,
     });
   });
 
@@ -43,9 +43,9 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
-      data: contact,
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
