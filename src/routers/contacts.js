63ea8016c562ae7c87cc927d7e6c37395f66bf45
maqss <maksymgulko@gmail.com>
2025-01-29 18:17:48 +0100

    remove roles check and age validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -14,45 +14,33 @@ import {
 } from '../validation/contacts.js';
 import { isValidId } from '../validation/isValid.js';
 import { authenticate } from '../middlewares/authenticate.js';
-import { checkRoles } from '../middlewares/checkRoles.js';
-import { ROLES } from '../constants/index.js';
 
 const contactsRouter = Router();
 
 contactsRouter.use(authenticate);
 
-contactsRouter.get(
-  '/',
-  // checkRoles(ROLES.ADMIN),
-  ctrlWrapper(getContactsController),
-);
+contactsRouter.get('/', ctrlWrapper(getContactsController));
 
 contactsRouter.get(
   '/:contactId',
-  checkRoles(ROLES.ADMIN, ROLES.USER),
-  checkRoles(ROLES.ADMIN, ROLES.USER),
   isValidId,
   ctrlWrapper(getContactByIdController),
 );
 
 contactsRouter.post(
   '/',
-  // checkRoles(ROLES.ADMIN),
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
 contactsRouter.delete(
   '/:contactId',
-  checkRoles(ROLES.ADMIN, ROLES.USER),
   isValidId,
   ctrlWrapper(deleteContactController),
 );
 
 contactsRouter.patch(
   '/:contactId',
-  checkRoles(ROLES.ADMIN, ROLES.USER),
-  checkRoles(ROLES.ADMIN, ROLES.USER),
   isValidId,
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,6 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   phoneNumber: Joi.string().min(3).max(30).required(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
-  age: Joi.number().integer().min(6).max(16).required(),
   email: Joi.string().min(3).max(30),
   isFavourite: Joi.boolean(),
   // userId: Joi.string()
@@ -16,7 +15,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   phoneNumber: Joi.string().min(3).max(30),
   contactType: Joi.string().valid('work', 'home', 'personal'),
-  age: Joi.number().integer().min(6).max(16),
   email: Joi.string().min(3).max(30),
   isFavourite: Joi.boolean(),
 });
