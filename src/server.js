7b89c3414180da6e30fc71c9f4b108e0fb66fcf9
maqss <maksymgulko@gmail.com>
2025-02-06 21:52:10 +0100

    make it start, at least

--- src/server.js
+++ src/server.js
@@ -35,13 +35,13 @@ export const setupServer = () => {
 
   app.use(router);
 
-  app.use('*', notFoundHandler);
-
-  app.use(errorHandler);
-
   app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use('/api-docs', swaggerDocs());
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
